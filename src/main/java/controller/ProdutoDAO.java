42eb6f25c2e8e5427cb8b41dcdf39e5dfc9e0a59
Tomásia Mussurí <tomasiamcm@gmail.com>
2023-12-18 21:59:00 -0300

    Add files via upload

--- src/main/java/controller/ProdutoDAO.java
+++ src/main/java/controller/ProdutoDAO.java
@@ -2,22 +2,18 @@ package controller;
 
 import model.Produto;
 
-import java.io.InputStream;
-import java.io.Reader;
-import java.math.BigDecimal;
-import java.net.URL;
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Scanner;
 
 public class ProdutoDAO {
     Connection conn;
-    public ProdutoDAO()throws SQLException {
+
+    public ProdutoDAO() throws SQLException {
         this.conn = ConexaoSQLite.getConexao();
     }
 
-    // select no banco de dados
+    // select
     public ArrayList<Produto> getAll() throws SQLException {
         ArrayList<Produto> produtos = new ArrayList<>();
         String sql = "SELECT * FROM produto;";
@@ -33,11 +29,25 @@ public class ProdutoDAO {
 
             produtos.add(produto);
         }
-
         return produtos;
     }
 
-    // insert no banco de dados
+    // select one
+    public Produto getOne(int id) throws SQLException {
+        String sql = "SELECT * FROM produto WHERE Id = ?;";
+        PreparedStatement preparedStatement = conn.prepareStatement(sql);
+        preparedStatement.setInt(1, id);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        Produto produto = new Produto();
+        produto.setId(resultSet.getInt("Id"));
+        produto.setNome(resultSet.getString("Nome"));
+        produto.setDescricao(resultSet.getString("Descricao"));
+        produto.setCategoria(resultSet.getString("Categoria"));
+        return produto;
+    }
+
+    // insert
     public void insert() throws SQLException {
         String sql = "INSERT INTO produto VALUES(null, ?, ?, ?);";
         PreparedStatement preparedStatement = conn.prepareStatement(sql);
@@ -56,4 +66,27 @@ public class ProdutoDAO {
     }
 
 
+    // update
+    public void update(int id, String campo, String valor) throws SQLException {
+        String sql = "UPDATE produto SET " + campo + " = " + valor + " ' WHERE Id = " + id + ";";
+        PreparedStatement preparedStatement = conn.prepareStatement(sql);
+        preparedStatement.setString(1, campo);
+        preparedStatement.setString(2, valor);
+        preparedStatement.setInt(3, id);
+        preparedStatement.executeUpdate();
+    }
+
+
+    // delet
+    public void delet() throws SQLException {
+        String sql = "DELETE FROM produto WHERE Id = ?;";
+        PreparedStatement preparedStatement = conn.prepareStatement(sql);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Informe o Id do produto: ");
+        int id = sc.nextInt();
+        preparedStatement.setInt(1, id);
+        preparedStatement.executeUpdate();
+
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,15 +5,80 @@ import model.Produto;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
         ProdutoDAO produtoDAO = new ProdutoDAO();
 
-        produtoDAO.insert();
+        Scanner sc = new Scanner(System.in);
+        int opcao = 0;
+
+        do {
+            System.out.println("***** ELETROTECH *****" +
+                    "\n[1] Produto" +
+                    "\n[2] Fornecedor");
+            System.out.println("Escolha a opção desejada: ");
+
+            opcao = sc.nextInt();
+
+            // menu produto
+            if (opcao == 1) {
+                int opcaoProduto = 0;
+                do {
+                    System.out.println("*** Cadastro de Produtos ***" +
+                            "\n[1] Listar" +
+                            "\n[2] Buscar" +
+                            "\n[3] Inserir" +
+                            "\n[4] Alterar" +
+                            "\n[5] Deletar");
+                    System.out.print("\nEscolha a opção desejada: ");
+                    opcao = sc.nextInt();
+                } while (opcaoProduto != 0);
+
+                if (opcaoProduto == 1) {
+                    produtoDAO.getAll();
+                } else if (opcaoProduto == 2) {
+                    System.out.println("Informe o id do produto: ");
+                    int id = sc.nextInt();
+                    produtoDAO.getOne(id = sc.nextInt());
+                } else if (opcaoProduto == 3) {
+                    produtoDAO.insert();
+                } else if (opcaoProduto == 4) {
+                    //produtoDAO.update();
+                } else if (opcaoProduto == 5) {
+                    produtoDAO.delet();
+                } else {
+                    System.out.println("Opção inválida <produto>, tente novamente!");
+                }
+
+
+            } else if (opcao == 2) {
+                int opcaoFornecedor = 0;
+                do{
+                    System.out.println("*** Cadastro de Fornecedores ***" +
+                            "\n[1] Listar" +
+                            "\n[2] Buscar" +
+                            "\n[3] Inserir" +
+                            "\n[4] Alterar" +
+                            "\n[5] Deletar");
+                    System.out.println("\nEscolha a opção desejada: ");
+                    opcaoFornecedor = sc.nextInt();
+                } while (opcaoFornecedor != 0);
+
+            } else {
+                System.out.println("Opção inválida, tente novamente!");
+            }
+        } while (opcao != 0);
+
 
         ArrayList<Produto> produtos = new ArrayList<>();
         //System.out.print(produtoDAO.getAll());
+
+        produtoDAO.insert();
+        //produtoDAO.update();
+        produtoDAO.delet();
+        //produtoDAO.getOne();
         produtos = produtoDAO.getAll();
 
         for (Produto produto:produtos) {
